67ebc539b7ab1ff6e5c3c00b841825b68b4f1c54
user1 <user1@user1-PC>
2020-03-19 15:51:28 +0900

    merge test2

---
+++ ourtrip/src/main/java/com/kh/ourtrip/admin/controller/AdminController.java
@@ -0,0 +1,28 @@
+package com.kh.ourtrip.admin.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.ourtrip.admin.model.service.AdminService;
+
+@Controller
+@RequestMapping("/admin/*")
+public class AdminController {
+
+	@Autowired
+	public AdminService adminService;
+	
+	// 관리자 main 페이지 이동 + 방문 데이터 조회
+	@RequestMapping("main")
+	public String adminMain() {
+		try {
+			Map<String, Integer> visitCounts = adminService.getVisitCount();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
