b50b9b415f8a009ee1d40913ae93e7a4f98d4994
usang0810 <jysrmb@gmail.com>
2020-03-23 17:50:07 +0900

    조유상 - 나의 플래너 목록 조회 백단 수정 중

--- ourtrip/src/main/java/com/kh/ourtrip/planner/controller/PlannerControllerJYS.java
+++ ourtrip/src/main/java/com/kh/ourtrip/planner/controller/PlannerControllerJYS.java
@@ -2,8 +2,10 @@ package com.kh.ourtrip.planner.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kh.ourtrip.member.model.vo.Member;
 import com.kh.ourtrip.planner.model.service.PlannerServiceJYS;
 
 @Controller
@@ -14,7 +16,21 @@ public class PlannerControllerJYS {
 	private PlannerServiceJYS plannerService;
 	
 	@RequestMapping("myPlanner")
-	public String myPlanner() {
+	public String myPlanner(Model model) {
+		
+		int memberNo = ((Member)model.getAttribute("loginMember")).getMemberNo();
+		
+		try {
+			// 수정 플래너 수 조회
+			int updatePlannerCount = plannerService.updatePlannerCount(memberNo);
+			
+			// 완료 플래너 수 조회
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			model.addAttribute("errorMsg", "나의 플래너 목록 조회 중 오류 발생");
+			return "common/errorPage";
+		}
 		
 		return "planner/myPlanner";
 	}
---
+++ ourtrip/src/main/java/com/kh/spring/common/vo/PageInfo.java
@@ -0,0 +1,90 @@
+package com.kh.spring.common.vo;
+
+public class PageInfo {
+	private int listCount; 		// 전체 게시글 수
+	private int limit; 			// 한 페이지에 보여질 게시글 수
+	private int pagingBarSize; 	// 보여질 페이징바의 페이지 개수
+	
+	private int currentPage; 	// 현재 페이지 번호를 표시할 변수
+	private int maxPage;	 	// 전체 페이지에서 가장 마지막 페이지
+	private int startPage;	 	// 페이징바 시작 페이지 번호
+	private int endPage; 	 	// 페이징바 끝 페이지 번호
+	
+	public PageInfo() {}
+
+	public PageInfo(int listCount, int limit, int pagingBarSize, int currentPage, int maxPage, int startPage,
+			int endPage) {
+		super();
+		this.listCount = listCount;
+		this.limit = limit;
+		this.pagingBarSize = pagingBarSize;
+		this.currentPage = currentPage;
+		this.maxPage = maxPage;
+		this.startPage = startPage;
+		this.endPage = endPage;
+	}
+
+	public int getListCount() {
+		return listCount;
+	}
+
+	public void setListCount(int listCount) {
+		this.listCount = listCount;
+	}
+
+	public int getLimit() {
+		return limit;
+	}
+
+	public void setLimit(int limit) {
+		this.limit = limit;
+	}
+
+	public int getPagingBarSize() {
+		return pagingBarSize;
+	}
+
+	public void setPagingBarSize(int pagingBarSize) {
+		this.pagingBarSize = pagingBarSize;
+	}
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+
+	public int getMaxPage() {
+		return maxPage;
+	}
+
+	public void setMaxPage(int maxPage) {
+		this.maxPage = maxPage;
+	}
+
+	public int getStartPage() {
+		return startPage;
+	}
+
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+
+	public int getEndPage() {
+		return endPage;
+	}
+
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
+
+	@Override
+	public String toString() {
+		return "PageInfo [listCount=" + listCount + ", limit=" + limit + ", pagingBarSize=" + pagingBarSize
+				+ ", currentPage=" + currentPage + ", maxPage=" + maxPage + ", startPage=" + startPage + ", endPage="
+				+ endPage + "]";
+	}
+	
+}
