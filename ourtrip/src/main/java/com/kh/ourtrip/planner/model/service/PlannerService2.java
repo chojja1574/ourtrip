b9786e71aa10992d30f4c65d2aad40d55770550a
cap515 <cap51515@gmail.com>
2020-03-23 14:21:33 +0900

    asd

--- ourtrip/src/main/java/com/kh/ourtrip/planner/model/service/PlannerService2.java
+++ ourtrip/src/main/java/com/kh/ourtrip/planner/model/service/PlannerService2.java
@@ -6,4 +6,6 @@ import com.kh.ourtrip.planner.model.vo.PlannerView;
 
 public interface PlannerService2 {
 	public abstract List<PlannerView> selectPlannerView(int no) throws Exception;
+	
+	//public abstract
 }
