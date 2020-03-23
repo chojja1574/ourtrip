bd4321ec16f41b87352d826bbfd9460180522927
user1 <user1@user1-PC>
2020-03-23 13:57:12 +0900

    0323

--- ourtrip/src/main/java/com/kh/ourtrip/common/HomeController.java
+++ ourtrip/src/main/java/com/kh/ourtrip/common/HomeController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.ourtrip.planner.model.service.PlannerServiceSDS;
+import com.kh.ourtrip.planner.model.vo.PlannerCard;
 
 
 @Controller
@@ -16,8 +18,18 @@ public class HomeController {
 	public PlannerServiceSDS plannerServiceSDS;
 	
 	@RequestMapping("/")
-	public String intro() {
-		
+	public String intro(Model model) {
+		try {
+			List<PlannerCard> recommendPCList = plannerServiceSDS.selectRecommendPCList();
+			System.out.println(recommendPCList);
+			if(!recommendPCList.isEmpty()) {
+				model.addAttribute("recommendPCList", recommendPCList);
+			} else {
+				model.addAttribute("msg", "조회내용이 없습니다");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		System.out.println("홈 이동 정상 동작");
 		return "index";
 	}
--- ourtrip/src/main/java/com/kh/ourtrip/planner/model/service/PlannerServiceImplSDS.java
+++ ourtrip/src/main/java/com/kh/ourtrip/planner/model/service/PlannerServiceImplSDS.java
@@ -1,8 +1,29 @@
 package com.kh.ourtrip.planner.model.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.kh.ourtrip.planner.model.dao.PlannerDAOSDS;
+import com.kh.ourtrip.planner.model.vo.PlannerCard;
 
 @Service
 public class PlannerServiceImplSDS implements PlannerServiceSDS{
+
+	@Autowired
+	public PlannerDAOSDS plannerDAOSDS;
+	
+	@Override
+	public List<PlannerCard> selectRecommendPCList() throws Exception {
+		List<PlannerCard> recommendPCList = plannerDAOSDS.selectRecommendPCList();
+//		List<Integer> rListNo = null;
+//		if(!recommendPCList.isEmpty()) {
+//			for(PlannerCard card : recommendPCList) {
+//				rListNo.card.getPlannerNo()
+//			}
+//		}
+		return recommendPCList;
+	}
 
 }
