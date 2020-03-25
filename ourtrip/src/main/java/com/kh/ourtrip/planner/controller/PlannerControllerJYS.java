fa3d38e256ecfbefd88c34f442e9e372ade934b6
usang0810 <jysrmb@gmail.com>
2020-03-25 15:30:40 +0900

    조유상 - 플래너 삭제, 나의 플래너 페이징 기능 수정

--- ourtrip/src/main/java/com/kh/ourtrip/planner/controller/PlannerControllerJYS.java
+++ ourtrip/src/main/java/com/kh/ourtrip/planner/controller/PlannerControllerJYS.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kh.ourtrip.member.model.vo.Member;
 import com.kh.ourtrip.planner.model.service.PlannerServiceJYS;
@@ -102,6 +103,30 @@ public class PlannerControllerJYS {
 		}
 		
 		return "planner/myPlanner";
+	}
+	
+	// 플래너 삭제
+	@RequestMapping("delPlanner")
+	public String delPlanner(PlannerMember delPlanner, Model model, RedirectAttributes rdAttr) {
+		int memberNo = ((Member)model.getAttribute("loginMember")).getMemberNo();
+		delPlanner.setMemberNo(memberNo);
+		
+		String msg = null;
+		try {
+			int result = plannerService.delPlanner(delPlanner);
+			
+			if(result > 0) msg = "플래너가 삭제되었습니다.";
+			else msg = "플래너 삭제에 실패하였습니다.";
+			
+			rdAttr.addFlashAttribute("msg", msg);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			model.addAttribute("errorMsg", "플래너 삭제 과정 중 오류 발생");
+			return "common/errorPage";
+		}
+		
+		return "redirect:myPlanner";
 	}
 
 }
